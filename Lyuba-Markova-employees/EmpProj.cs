1929fdf86a0f0b59ef2ff3e2727021a3d89dace8
IonCannon <IonCannon@DESKTOP-46RABP1>
2023-04-05 18:25:10 +0300

    Add project files.

---
+++ Lyuba-Markova-employees/EmpProj.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lyuba_Markova_employees
+{
+    internal class EmpProj
+    {
+        public int EmpId;
+        public int ProjectId;
+        public DateTime DateFrom;
+        public DateTime? DateTo;
+
+        public static EmpProj FromCsv(string csvLine, string separator)
+        {
+            string[] values = csvLine.Split(separator);
+            EmpProj empProj = new EmpProj();
+            empProj.EmpId = int.Parse(values[0]);
+            empProj.ProjectId = int.Parse(values[1]);
+            empProj.DateFrom = DateTime.Parse(values[2]);
+            if (values[3].Trim().ToLower().Equals("null"))
+                empProj.DateTo = null;
+            else
+                empProj.DateTo = DateTime.Parse(values[3]);
+
+            return empProj;
+        }
+    }
+}
---
+++ Lyuba-Markova-employees/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lyuba_Markova_employees
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                List<EmpProj> values = File.ReadAllLines(path: openFileDialog.FileName)
+                                              .Select(v => EmpProj.FromCsv(v, ","))
+                                              .ToList();
+                int p = 4;
+            }
+        }
+    }
+}
